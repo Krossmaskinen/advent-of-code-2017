6053f82c3bfae1b9b58cdf36d7dbe7ef4e008094
Bill Kurtson <bill.kurtson@innofactor.com>
2017-12-06 10:47:58 +0100

    day 6 - 2 complete

--- day6/main.js
+++ day6/main.js
@@ -3,6 +3,9 @@ let inputFile = './input.txt';
 let input = fs.readFileSync(inputFile, 'utf8');
 let memoryBanks;
 let iterations = 0;
+let foundDouble = false;
+let lastSeenCyclesAgo = 0;
+let lookForCombination;
 
 let checkedCombinations = [];
 
@@ -32,6 +35,9 @@ function redistribute() {
     }
 
     iterations++;
+    if (foundDouble) {
+        lastSeenCyclesAgo++;
+    }
 }
 
 function checkIfExists(combination) {
@@ -39,27 +45,58 @@ function checkIfExists(combination) {
         let isSame = true;
         let checkedCombo = checkedCombinations[i];
 
-        for (let j = 0; j < checkedCombo.length; ++j) {
-            if (checkedCombo[j] !== combination[j]) {
-               isSame = false;
-               break;
-            }
-        }
-
-        if (isSame)
+        if (compareArrays(checkedCombo, combination))
             return true;
     }
+}
+
+function compareArrays(arr1, arr2) {
+    for (let i = 0; i < arr1.length; ++i) {
+        if (arr1[i] !== arr2[i]) {
+           return false;
+        }
+    }
+
+    return true;
 }
 
 function copyObject(obj) {
     return JSON.parse(JSON.stringify(obj));
 }
 
-memoryBanks = formatInput(input);
+function solvePart1() {
+    memoryBanks = formatInput(input);
 
-while(!checkIfExists(memoryBanks)) {
-    checkedCombinations.push(copyObject(memoryBanks));
-    redistribute();
+    while(!checkIfExists(memoryBanks)) {
+        checkedCombinations.push(copyObject(memoryBanks));
+        redistribute();
+    }
+
+    console.log(`double found after ${iterations} cycles`);
 }
 
-console.log(`iterations: ${iterations}`);
+function solvePart2() {
+    // input = '0 2 7 0';
+    memoryBanks = formatInput(input);
+    iterations = 0;
+    checkedCombinations.length = 0;
+
+    while(true) {
+        checkedCombinations.push(copyObject(memoryBanks));
+        redistribute();
+
+        if (!foundDouble) {
+            if (checkIfExists(memoryBanks)) {
+                foundDouble = true;
+                lookForCombination = copyObject(memoryBanks);
+            }
+        } else if (compareArrays(memoryBanks, lookForCombination)) {
+            break;
+        }
+    }
+
+    console.log(`last seen ${lastSeenCyclesAgo} cycles ago`);
+}
+
+solvePart1();
+solvePart2();
