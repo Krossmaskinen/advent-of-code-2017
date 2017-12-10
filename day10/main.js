87b8d62e0116d8a1465d1f2dbf1e22b3f43c7069
Bill <bill.kurtson@gmail.com>
2017-12-10 19:02:03 +0100

    day 10 - 2 in progress

--- day10/main.js
+++ day10/main.js
@@ -1,20 +1,28 @@
 var fs = require('fs');
 var inputFile = 'input.txt';
-var input = fs.readFileSync(inputFile, 'utf8');
-var hashList = getHashList(256);
+var baseInput = fs.readFileSync(inputFile, 'utf8');
 var skipSize = 0;
 var currentPos = 0;
 let result;
+let extraLengths = [17, 31, 73, 47, 23];
+let input;
+let input2;
+let hashLength = 256;
 
-input = formatInput(input);
-
-// input = [3, 4, 1, 5];
-// hashList = [0, 1, 2, 3, 4];
-
-console.log(hashList[255]);
+sparseHash = getHashList(hashLength);
+input = formatInput(baseInput);
+input2 = formatInput2(baseInput);
 
 function formatInput(input) {
     return input.split(',').map(Number);
+}
+
+function formatInput2(input, splitChar) {
+    let formattedInput = input.split('').map(num => num.charCodeAt());
+
+    formattedInput.push(...extraLengths);
+
+    return formattedInput;
 }
 
 function getHashList(size) {
@@ -33,48 +41,149 @@ function step(totalLength) {
     let totalSelection;
     let length1 = totalLength;
     let length2;
-    let part1;
-    let part2;
-    let diff = hashList.length - (currentPos + totalLength);
+    let diff = sparseHash.length - (currentPos + totalLength);
 
     if (diff < 0) {
-        length1 = hashList.length - currentPos;
+        length1 = sparseHash.length - currentPos;
         length2 = Math.abs(diff);
     }
 
-    selection1 = hashList.slice(currentPos, currentPos + length1);
+    selection1 = sparseHash.slice(currentPos, currentPos + length1);
     totalSelection = selection1;
 
     if (length2) {
-        selection2 = hashList.slice(0, length2);
+        selection2 = sparseHash.slice(0, length2);
         selection1.push(...selection2); // combine selection 1 and 2
     }
 
     totalSelection = totalSelection.reverse();
-    part1 = totalSelection.splice(0, length1);
 
-    hashList.splice(currentPos, length1, ...part1);
+    insertResult(totalSelection, length1, length2)
+    updatePosition(totalLength);
+}
+
+function insertResult(selection, length1, length2) {
+    let part1 = selection.splice(0, length1);
+    let part2;
+
+    sparseHash.splice(currentPos, length1, ...part1);
 
     if (length2) {
-        part2 = totalSelection;
-        hashList.splice(0, length2, ...part2);
+        part2 = selection;
+        sparseHash.splice(0, length2, ...part2);
+    }
+}
+
+function updatePosition(length) {
+    currentPos += length + skipSize++;
+
+    if (currentPos >= sparseHash.length) {
+        currentPos = currentPos - sparseHash.length;
+    }
+}
+
+function getDenseHash(sparseHash) {
+    let tempHash = [];
+    let segments = [];
+    let segment;
+
+    for (let i = 0; i < 16; ++i) {
+        segment = sparseHash.slice(i * 16, (i + 1) * 16);
+        segments.push(segment);
     }
 
-    currentPos += totalLength + skipSize++;
+    for (segment of segments) {
+        segment = segment.reduce((prev, cur) => {
+            return prev ^ cur;
+        }, 0)
 
-    if (currentPos >= hashList.length) {
-        currentPos = currentPos - hashList.length;
+        tempHash.push(segment);
     }
+
+    return tempHash.map(num => {
+        num = num.toString(16);
+        if (num.length === 1) {
+            num = '0' + num;
+        }
+
+        return num;
+    }).join('');
 }
 
 function getResult() {
-    return hashList[0] * hashList[1];
+    return sparseHash[0] * sparseHash[1];
 }
 
-for (let instruction of input) {
-    step(instruction);
+function reset() {
+    skipSize = 0;
+    currentPos = 0;
+    result = 0;
+    sparseHash = getHashList(hashLength);
 }
 
-result = getResult();
+function solve1() {
+    for (let instruction of input) {
+        step(instruction);
+    }
 
-console.log(`result: ${result}`);
+    result = getResult();
+    console.log(`result: ${result}`);
+}
+
+function solve2() {
+    let denseHash;
+
+    input2 = extraLengths;
+
+    for (let i = 0; i < 64; ++i) {
+        for (let instruction of input2) {
+            step(instruction);
+        }
+    }
+
+    denseHash = getDenseHash(sparseHash);
+    console.log(`result: ${denseHash}`);
+}
+
+// solve1();
+// reset();
+// solve2();
+test();
+
+function test() {
+    let temp;
+    let input2 = formatInput2('');
+
+    for (let i = 0; i < 64; ++i) {
+        for (let instruction of input2) {
+            step(instruction);
+        }
+    }
+
+    let segments = [];
+    let segment;
+
+    for (let i = 0; i < 16; ++i) {
+        segment = sparseHash.splice(0, 16);
+
+        segment = segment.reduce((prev, cur) => {
+            return prev ^ cur;
+        }, 0);
+
+        segments.push(segment);
+    }
+
+
+    segments = segments.map(num => {
+        num = num.toString(16);
+        if (num.length === 1) {
+            num = '0' + num;
+        }
+
+        return num;
+    }).join('');
+
+    console.log('temp hash  ', segments);
+    console.log('should be  ', '3efbe78a8d82f29979031a4aa0b16a9d');
+}
+
